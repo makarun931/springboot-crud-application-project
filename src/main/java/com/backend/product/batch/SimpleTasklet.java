f70cb904707cebd2339a6ef957f89ead538a967b
ARUN KUMAR M <105279341+makarun931@users.noreply.github.com>
2023-11-27 12:18:41 +0530

    Add files via upload

---
+++ src/main/java/com/backend/product/batch/SimpleTasklet.java
@@ -0,0 +1,15 @@
+package com.backend.product.batch;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class SimpleTasklet implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("Executing simple tasklet...");
+      
+        return RepeatStatus.FINISHED;
+    }
+}
